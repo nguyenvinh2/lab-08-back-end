72cbe33a11bcfdac6930907f348d9de0e6d45397
Vinh Nguyen <nguyenv2@outlook.com>
2018-09-20 09:43:05 -0700

    initial share

---
+++ schema.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE IF NOT EXISTS locations ( 
+  id SERIAL PRIMARY KEY, 
+  search_query VARCHAR(255), 
+  formatted_query VARCHAR(255), 
+  latitude NUMERIC(8, 6), 
+  longitude NUMERIC(9, 6) 
+);
+
+CREATE TABLE IF NOT EXISTS weathers ( 
+  id SERIAL PRIMARY KEY, 
+  forecast VARCHAR(255), 
+  time VARCHAR(255), 
+  created_at BIGINT,
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
+);
+
+CREATE TABLE IF NOT EXISTS yelp ( 
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255),
+  image_url VARCHAR(255),
+  price VARCHAR(4),
+  rating NUMERIC(4,2),
+  url VARCHAR(255),
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
+);
+
+CREATE TABLE IF NOT EXISTS movies ( 
+  id SERIAL PRIMARY KEY, 
+  title VARCHAR(255), 
+  overview VARCHAR(5000), 
+  average_votes NUMERIC(4,2),
+  total_votes NUMERIC(10000,0),
+  image_url VARCHAR(255),
+  popularity NUMERIC(4,2),
+  released_on VARCHAR(255),
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
+);
