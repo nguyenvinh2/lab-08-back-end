e9814874a00500551648fd39bdc410d589166d39
dhwang21 <hwang.derrick@gmail.com>
2018-09-21 11:14:11 -0700

    finished meetup

--- schema.sql
+++ schema.sql
@@ -32,10 +32,35 @@ CREATE TABLE IF NOT EXISTS movies (
   title VARCHAR(255), 
   overview VARCHAR(5000), 
   average_votes NUMERIC(4,2),
-  total_votes NUMERIC(1000,0),
+  total_votes NUMERIC(65,0),
   image_url VARCHAR(255),
   popularity NUMERIC(4,2),
   released_on VARCHAR(255),
   created_at BIGINT,
   location_id INTEGER NOT NULL REFERENCES locations(id) 
 );
+
+CREATE TABLE IF NOT EXISTS meetups (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  link VARCHAR(255),
+  creation_date VARCHAR(255),
+  host VARCHAR(255),
+  created_at BIGINT,
+  location_id INTEGER NOT NULL REFERENCES locations(id)
+)
+
+CREATE TABLE IF NOT EXISTS trails (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  length NUMERIC(4,2),
+  stars NUMERIC(4,2),
+  star_votes NUMERIC(255,0)
+  summary VARCHAR(255),
+  trail_url VARCHAR(255),
+  conditions VARCHAR(255),
+  condition_date VARCHAR(255),
+  condition_time VARCHAR(255),
+  created_at BIGINT,
+  location_id INTEGER NOT NULL REFERENCES location(id)
+)
